e7318a7c4fb72cd416b2dcd4aa1fd4f6d670f99b
dsjdd <90551688+dsjdd@users.noreply.github.com>
2023-07-11 18:11:43 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <h2>todoList</h2>
+  <todoInput @addTask="addTask"></todoInput>
+  <todoList :todoList="todoList"></todoList>
+  <todoButton @changeActive="changeActive"></todoButton>
+  
+</template>
+
+<script>
+import todoList from './components/todoList.vue';
+import todoButton from './components/todoButton.vue';
+import todoInput from './components/todoInput.vue';
+export default {
+  name:"App",
+  data(){
+    return{
+      todoList:[
+        {id:1,task:'吃饭',isCompleted:true},
+        {id:2,task:'睡觉',isCompleted:false},
+        {id:3,task:'打豆豆',isCompleted:false},
+      ],
+      active:0
+    }
+  },
+  computed:{
+    todoList(){
+      if(this.active===0){
+        // console.log(1);
+        return this.todoList;
+      }
+      else if(this.active===1){
+        return this.todoList.filter((item)=>item.isCompleted);
+      }
+      else{
+        return this.todoList.filter((item)=>!item.isCompleted);
+      }
+    }
+  },
+  methods:{
+    changeActive(active){
+      this.active=active;
+      // console.log(this.active)
+    },
+    addTask(taskName){
+      this.todoList.push({
+        id:this.todoList.length+1,
+        task:taskName,
+        isCompleted:false,
+      })
+      
+    }
+  },
+  components:{
+    todoList,
+    todoButton,
+    todoInput
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/todoInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">新任务</span>
+    <input type="text" class="form-control" placeholder="请输入任务" aria-label="Username" aria-describedby="basic-addon1" v-model="taskName">
+    <button class="btn-primary" @click="onsubmit">提交</button>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"todoInput",
+    data(){
+        return{
+            taskName:''
+        }
+    },
+    emits:['addTask'],
+    methods:{
+        onsubmit(){
+            this.$emit('addTask',this.taskName),
+            this.taskName=''
+        }
+    }
+}
+</script>
+
+<style scoped>
+.input-group{
+    width: 500px;
+    margin:0 auto;
+}
+</style>
